ad0481f514da67adcc587a415eaf26469a0bb3b8
kasiranaweera <ranaweerakasi@gmail.com>
2026-01-23 14:14:09 +0530

    fix: update postcss and tailwind configuration for module exports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
@@ -188,5 +189,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [animatePlugin],
 } satisfies Config;
